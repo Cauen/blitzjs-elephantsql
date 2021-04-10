07a216eda022a34a0159c9105a731620629e7f9a
CAUEN <emanuel.caue@gmail.com>
2021-04-10 11:15:59 -0300

    ci: update prisma connection

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,38 @@
+import { Prisma } from 'db';
 import {enhancePrisma} from "blitz"
 import {PrismaClient} from "@prisma/client"
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaType;
+    }
+  }
+}
+
+interface EnhancedPrismaClientAddedMethods {
+  $reset: () => Promise<void>;
+}
+
+type PrismaType = PrismaClient<Prisma.PrismaClientOptions, unknown, unknown> & EnhancedPrismaClientAddedMethods
+
 const EnhancedPrisma = enhancePrisma(PrismaClient)
+let prisma: PrismaType
+let total: number = 0
+
+console.log({ globalPrisma: global.prisma })
+global.prisma = (() => {
+  if (global.prisma) {
+    total = total + 1
+    console.log("GETTING FROM CACHE " + total)
+  }
+  return global.prisma
+})() || (() => {
+  total = total + 1
+  console.log("CREATING PRISMA ENHANCED " + total)
+  return new EnhancedPrisma()
+})()
+prisma = global.prisma
 
 export * from "@prisma/client"
-export default new EnhancedPrisma()
+export default prisma
